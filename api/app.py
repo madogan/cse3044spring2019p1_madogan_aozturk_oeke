b7ae7436ae2ff27edd83443199eb7ea3625bc9bf
madogan <muhammed.ali.dogan@outlook.com>
2019-05-25 14:43:22 +0300

    ADD: Register Form

--- api/app.py
+++ api/app.py
@@ -1,11 +1,46 @@
-from flask import Flask, request
+from flask import Flask, request, render_template, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
+from wtforms import Form, BooleanField, StringField, PasswordField, validators, IntegerField
+
 
 db = SQLAlchemy()
 
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://root:admin@localhost:5432/allaamus_db'
+
+
+class RegistrationForm(Form):
+    first_name = StringField('First Name', [
+        validators.Length(min=3, max=25),
+        validators.DataRequired()
+    ])
+    last_name = StringField('Last Name', [
+        validators.Length(min=3, max=25),
+        validators.DataRequired()
+    ])
+    age = IntegerField('Age', [
+        validators.DataRequired(),
+        validators.DataRequired()
+    ])
+    job = StringField('Job', [
+        validators.Length(min=4, max=25),
+        validators.DataRequired()
+    ])
+    password = PasswordField('New Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
+    email = StringField('Email Address', [
+        validators.Length(min=6, max=35),
+        validators.DataRequired(),
+        validators.Email()
+    ])
+    phone = StringField('Phone Number', [
+        validators.Length(min=10, max=16),
+        validators.DataRequired()
+    ])
+    accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
 
 
 class User(db.Model):
@@ -15,26 +50,37 @@ class User(db.Model):
     age = db.Column(db.Integer, nullable=False)
     job = db.Column(db.String, nullable=False)
     email = db.Column(db.String(192), unique=True, nullable=False)
-    phone = db.Column(db.String(10), unique=True, nullable=False)
+    phone = db.Column(db.String(16), unique=True, nullable=False)
     activation = db.Column(db.Boolean, unique=False, default=False)
 
     def __repr__(self):
         return f'{self.id}: {self.first_name} {self.last_name} (Activation Status: {self.activation})'
 
 
-@app.route('/login', method=['POST'])
-def login():
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm(request.form)
     if request.method == 'POST':
-        if __name__ == '__main__':
-            email = request.json.get("email", None)
-            password = request.json.get("password", None)
+        if form.validate():
+            new_user = User(
+                first_name=form.first_name.data,
+                last_name=form.last_name.data,
+                age=form.age.data,
+                job=form.job.data,
+                email=form.email.data,
+                phone=form.phone.data,
+                activation=True
+            )
+            db.session.add(new_user)
+            flash('Thanks for registering')
+            return redirect(url_for('/login'))
+    return render_template('register.html', form=form)
 
-            if email is None:
-                return
 
+@app.route('/login', methods=['POST'])
+def login():
     return 'Login'
 
 
-@app.route('/register', method=['POST'])
-def register():
-    return 'Register'
