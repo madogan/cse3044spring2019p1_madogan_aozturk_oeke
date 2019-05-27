eb26f8c4b324d12b84e73020a4b4f909961d323d
madogan <muhammed.ali.dogan@outlook.com>
2019-05-27 14:06:58 +0300

    ADD: Login and register pages are added.

--- allaamus/app/routes.py
+++ allaamus/app/routes.py
@@ -1,19 +1,32 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, flash, redirect
+from app.forms import LoginForm
 
 
-@app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    user = {'username': 'Miguel'}
-    return render_template('index.html', title='Anasayfa - Allaamus', user=user)
+    return render_template('index.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
-
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.email.data, form.remember_me.data))
+        return redirect('/')
+    return render_template('login.html', form=form)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    return 'Register'
+    return render_template('register.html')
+
+
+@app.errorhandler(404)
+def error(e):
+    """
+        TODO:
+            * Repeat this for other error types.
+                (http://flask.pocoo.org/docs/1.0/patterns/errorpages/)
+    """
+    return render_template('error.html', error={'code': 404, 'message': "Not Found!"})
