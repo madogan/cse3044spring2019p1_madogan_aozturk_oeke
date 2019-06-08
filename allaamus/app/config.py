3b0d696e41c04fcb5f69c38189f95a759dbe8178
madogan <muhammed.ali.dogan@outlook.com>
2019-06-08 15:46:33 +0300

    UPDATE: Database URI from environ.

--- allaamus/app/config.py
+++ allaamus/app/config.py
@@ -2,12 +2,13 @@ import os
 
 class BaseConfig(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'bilemezsin_kiii'
-    SQLALCHEMY_DATABASE_URI = 'postgres://root:admin@localhost:5432/allaamus_db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgres://madogan:admin@localhost:5432/allaamus_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
+
 
 class ProductionConfig(BaseConfig):
     DEBUG = False
