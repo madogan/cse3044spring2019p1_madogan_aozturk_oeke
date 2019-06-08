3314334236a3f18596116470c3e0565a1fd9aa54
madogan <muhammed.ali.dogan@outlook.com>
2019-06-08 16:44:58 +0300

    UPDATE: index.html jinja format.

--- allaamus/app/__init__.py
+++ allaamus/app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_migrate import Migrate
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from app.config import DevelopmentConfig
 
@@ -8,5 +9,6 @@ app.config.from_object(DevelopmentConfig)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login_manager = LoginManager(app)
 
 from app import routes, models
