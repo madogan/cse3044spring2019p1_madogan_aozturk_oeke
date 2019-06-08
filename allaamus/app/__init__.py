453b39df6b45f76d8de029bb80910c2b808102ee
madogan <muhammed.ali.dogan@outlook.com>
2019-06-08 15:47:48 +0300

    ADD: Databsse migration mechanism.

--- allaamus/app/__init__.py
+++ allaamus/app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
-from app.config import DevelopmentConfig
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from app.config import DevelopmentConfig
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes
+from app import routes, models
--- allaamus/app/routes.py
+++ allaamus/app/routes.py
@@ -2,7 +2,8 @@ from app import app
 from os.path import join
 from app.forms import LoginForm, RegisterForm
 from flask import render_template, request, flash, redirect
-
+from app.models import *
+from app import db
 
 @app.route('/index', methods=['GET', 'POST'])
 @app.route('/', methods=['GET', 'POST'])
@@ -12,8 +13,32 @@ def index():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    """
+        TODO:
+            * Hata mesajı döndürme mekanizması eklenecek.
+    """
     register_form = RegisterForm()
-    return render_template("auth/register.html", title="Kayıt", form=register_form)
+    if request.method == "POST":
+        if register_form.validate_on_submit():
+            new_user = User(
+                first_name = register_form.first_name.data,
+                last_name = register_form.last_name.data,
+                email = register_form.email.data,
+                password_hash = register_form.password.data,
+                user_type = register_form.user_type.data
+            )
+            if User.query.get(new_user) is not None:
+                
+                db.session.add(new_user)
+                db.session.commit()
+            else:  # user exist
+                render_template('error/error.html', error={'code': 31, 'message': "User exist"})
+            return redirect('login')
+        else:
+            return render_template("auth/register.html", title="Kayıt", form=register_form)
+    if request.method == "GET":
+        return render_template("auth/register.html", title="Kayıt", form=register_form)
+    return render_template('error/error.html', error={'code': 404, 'message': "Not Found!"})
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
